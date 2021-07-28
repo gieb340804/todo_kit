4e76775b329d834b1b37f6afdc1e80e99deb5ada
Gleb_Domahnev <gieb340804m3@gmail.com>
2021-07-28 16:14:55 +0700

    one commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import List from './componets/List'
+
+
+
+function App() {
+  return (
+  <div className="todo">
+    <div className="todo_sidebar">
+      <List/>
+    
+      </div>
+  </div>
+  );
+}
+  
+export default App;
