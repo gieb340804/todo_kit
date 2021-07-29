f1af60a8b5a318bfeab645f8238c5d8067a4aab1
Gleb_Domahnev <gieb340804m3@gmail.com>
2021-07-29 13:41:33 +0700

    thre_commit

---
+++ src/componets/List/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './List.scss'
+
+const List = ({ items }) =>{
+    return(
+        <ul className="list">
+            {
+                items.map(item  => (
+            <li className={item.active ? 'active' : ''}>
+             <i>{item.icon ? (item.icon) : (<i className={`badge badge--${item.color}`}></i>)}
+            </i>
+            <span>{item.name}</span>
+            </li>
+            ))}
+        </ul>
+    );
+};
+
+export default List;
